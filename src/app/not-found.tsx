c41d905283897580ef418127c26bb56039c3a729
vishva-kalhara <vishvakalhara@gmail.com>
2025-04-11 15:25:32 +0530

    Page message

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import PageMessage from "@/components/page-message";
+
+const Page = () => {
+    return (
+        <PageMessage
+            tag="Not Found"
+            title="Page Not found"
+            description="This page isn't where you left it. Try checking the link or head back home."
+            action={{
+                text: "Back to Home",
+                link: "/",
+            }}
+        />
+    );
+};
+
+export default Page;
--- src/components/forbidden.tsx
+++ src/components/forbidden.tsx
@@ -1,35 +1,21 @@
-import Link from "next/link";
-import { Button } from "./ui/button";
+import PageMessage from "./page-message";
 
 const ForbiddenCard = () => {
     return (
-        <div className="max-w-lg mx-auto flex  justify-center flex-col mb-10 py-8">
-            <div className="w-full justify-center flex">
-                <span className="font-semibold text-xs text-blue-500 py-1 px-2 bg-blue-100 border border-blue-300 rounded-md">
-                    Forbidden
-                </span>
-            </div>
-            <h1 className="font-semibold text-2xl text-center mt-4">
-                Access Restricted
-            </h1>
-            <p className="text-base text-center text-black/50 mt-2">
-                This page is reserved for Handy Man members. Please sign in to
-                view bids, post tasks, or continue helping your community.
-            </p>
-            <div className="flex gap-2 justify-center mt-10">
-                <Link href="/" className="flex justify-center">
-                    <Button
-                        variant={"outline"}
-                        className="bg-white/70! hover:bg-white!"
-                    >
-                        Back to Home
-                    </Button>
-                </Link>
-                <Link href="/auth/sign-in" className="flex justify-center">
-                    <Button>Sign In Now</Button>
-                </Link>
-            </div>
-        </div>
+        <PageMessage
+            tag="Forbidden"
+            title="Access Restricted"
+            description="This page is reserved for Handy Man members. Please sign in to
+            view bids, post tasks, or continue helping your community."
+            action={{
+                text: "Sign In Now",
+                link: "/auth/sign-in",
+            }}
+            fallback={{
+                text: "Back to Home",
+                link: "/",
+            }}
+        />
     );
 };
 
