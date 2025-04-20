2a9b3b739642fdf4c49add204e27f847b641552b
vishva-kalhara <vishvakalhara@gmail.com>
2025-04-20 08:00:52 +0530

    updated base url

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
         return [
             {
                 source: "/api/v1/:path*",
-                destination: "http://localhost:5000/api/v1/:path*",
+                destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
             },
         ];
     },
