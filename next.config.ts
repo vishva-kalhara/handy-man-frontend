6b89cbe4f78955563a98fbc89d8b90339b37cc83
vishva-kalhara <vishvakalhara@gmail.com>
2025-04-20 10:18:39 +0530

    removed unwanted image sources

--- next.config.ts
+++ next.config.ts
@@ -13,39 +13,10 @@ const nextConfig: NextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "res.cloudinary.com",
-            },
-            {
-                protocol: "https",
-                hostname: "thumbs.dreamstime.com",
-            },
-            {
-                protocol: "https",
-                hostname: "t3.ftcdn.net",
-            },
-            {
-                protocol: "https",
-                hostname: "geediting.com",
-            },
-            {
-                protocol: "https",
-                hostname: "media.designcafe.com",
-            },
-            {
-                protocol: "https",
-                hostname: "whatsheethinks.wordpress.com",
-            },
-            {
-                protocol: "https",
-                hostname: "media.gettyimages.com",
-            },
-            {
-                protocol: "https",
                 hostname: "handy-man-storage-prod.s3.amazonaws.com",
             },
         ],
     },
-    /* config options here */
 };
 
 export default nextConfig;
